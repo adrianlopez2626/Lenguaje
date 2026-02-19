b151938057ba6c5bf0b095ebde0728fb07e46bf1
adrianlopez2626 <adrianlona4@gmail.com>
2026-02-19 11:16:56 +0100

    actividades

---
+++ Ejercicio_21_JS_Basico_Manipulacion_DOM/script.js
@@ -0,0 +1,19 @@
+// Ejercicio 21: Manipulación del DOM
+
+// 1. Selecciona la caja (id="miCaja")
+
+
+// 2. Selecciona los botones
+
+
+// 3. Agrega funcionalidad al botón de color (click)
+//    - Cambia el color de fondo de la caja
+
+
+// 4. Agrega funcionalidad al botón de texto (click)
+//    - Cambia el texto interno de la caja
+
+
+// 5. Agrega funcionalidad al botón de agregar (click)
+//    - Crea un nuevo elemento li
+//    - Agrégalo a la lista ul
---
+++ Ejercicio_22_JS_Eventos_Clases/script.js
@@ -0,0 +1,10 @@
+// Ejercicio 22: Eventos y Clases
+
+// 1. Selecciona el botón del menú, el botón de cerrar y el menú lateral
+
+
+// 2. Define una función 'toggleMenu' que:
+//    - Use classList.toggle('hidden') en el menú
+
+
+// 3. Agrega los Event Listeners a los botones para llamar a esa función
